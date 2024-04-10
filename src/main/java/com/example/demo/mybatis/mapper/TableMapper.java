f7f6f280987c85be57b9f6dbb8423574f7b8bede
Lê Ngọc Hưng <hung.le@flex.com.vn>
2024-04-10 18:41:53 +0700

    i

--- src/main/java/com/example/demo/mybatis/mapper/TableMapper.java
+++ src/main/java/com/example/demo/mybatis/mapper/TableMapper.java
@@ -25,7 +25,7 @@ public interface TableMapper {
                 "\tLEFT JOIN (\r\n" + //
                 "\t\tSELECT * FROM information_schema.statistics s WHERE table_schema = 'flex_anphu' AND table_name = #{table_name}\r\n" + //
                 "\t) s ON c.TABLE_NAME = s.TABLE_NAME AND c.COLUMN_NAME = s.COLUMN_NAME\n" + //
-                "LEFT JOIN ( SELECT * FROM information_schema.statistics s WHERE table_schema = 'flex_anphu') t ON c.COLUMN_NAME = t.COLUMN_NAME AND c.TABLE_NAME != t.TABLE_NAME\n" +
+                "LEFT JOIN ( SELECT * FROM information_schema.COLUMNS s WHERE table_schema = 'flex_anphu') t ON c.COLUMN_NAME = t.COLUMN_NAME AND c.TABLE_NAME != t.TABLE_NAME\n" +
                 "WHERE\n" + //
                 "\tc.TABLE_SCHEMA = 'flex_anphu' \n" + //
                 "\tAND c.TABLE_NAME = #{table_name} GROUP BY c.COLUMN_NAME ORDER BY IFNULL(c.COLUMN_KEY, 'Y') DESC, c.COLUMN_NAME ASC;")
